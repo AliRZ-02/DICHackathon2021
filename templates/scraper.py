bc095763a5e1fe73d2b0b8ab1a9fd291e79351d0
AliRZ-02 <alirzaidi02@gmail.com>
2021-03-12 22:25:42 -0500

    Web Scraped City Names

--- templates/scraper.py
+++ templates/scraper.py
@@ -4,37 +4,33 @@ import requests
 from bs4 import BeautifulSoup
 
 province = requests.get(
-	url="https://en.wikipedia.org/wiki/List_of_municipalities_in_Alberta",
+	url="https://en.wikipedia.org/wiki/List_of_municipalities_in_Yukon",
 )
 
 soup = BeautifulSoup(province.content, 'html.parser')
-links = soup.find(id = "bodyContent").find_all("a")
+links = soup.find(id == "bodyContent").find_all('a')
 
 counter = 0
+vcount = 0
 list2 = False
 provinceList = []
-
 for link in links:
     ref = str(link.string)
-    if ref == 'Acme' or ref == 'MD of Acadia No. 34':
+    print(ref)
+    if ref == 'Carmacks':
         list2 = True
-        counter += 1
-    elif ref == 'Youngstown' or ref == 'Wood Buffalo, Regional Municipality of' and list:
-        list2 = False
-    elif ref == 'Yellowhead County' and list:
-        list2 = False
+    elif ref == 'Whitehorse' and list2:
+        provinceList.append(ref)
         break
-    elif ref == 'Crowsnest Pass, Municipality of' and counter != 0:
-        list = True
     if ref != "None" and ref[0] != '[' and list2:
         provinceList.append(ref)
-
 print(provinceList)
 provinces = set(provinceList)
 provinceList = sorted(provinces)
+print(provinceList)
 print(len(provinceList))
 
-with open('Alberta.csv', 'w' ,newline="", encoding = "utf-8") as f:
+with open('YK.csv', 'w' ,newline="", encoding = "utf-8") as f:
     writer = csv.writer(f,quoting=csv.QUOTE_ALL)
     for word in provinceList:
         writer.writerow([word])
